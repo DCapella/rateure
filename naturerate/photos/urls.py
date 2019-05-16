224e014311a6e6783dafc8f999a31002ee201a46
David Capella <D_Capella@yahoo.com>
2019-05-16 14:24:18 -0500

    delete

--- naturerate/photos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import Home, CreatePhotoView
-
-urlpatterns = [
-    path('', Home.as_view(), name='home'),
-    path('photo/', CreatePhotoView.as_view(), name='add_photo'),
-]
